a0fafdd81c2428cc1fd6bb77b4f15ac01a3daa5b
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-05-14 15:38:46 -0400

    Updated Card Modal

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -4,6 +4,7 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 var openTask = document.getElementById("addTask");
 var closeTask = document.getElementById("closeButton");
 var cardTask = document.getElementById("newTask");
+var dateTask = document.getElementById("dateInput");
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
@@ -16,101 +17,92 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard() {
+  const Title = [];
+  let currentTitles = JSON.parse(localStorage.getItem('Title'));
+  
+  if (currentTitles == null) {
+    currentTitles = [];
+  };
+  
+  if (currentTitles.length >= 5) {
+    currentTitles.length = 0;
+  };
+  
+  const newTitle= document.getElementById('titleInput').value;
+  
+  if (newTitle == "") {
+    submit.preventDefault();
+    alert("Please enter your Task Title");}
+  
+    else {
+  if (currentTitles === null) {
+    Title.push(newTitle);
+    localStorage.setItem('Title', JSON.stringify(Title));
+  
+  } else {
+  currentTitles.push(newTitle);
+  localStorage.setItem('Title', JSON.stringify(currentTitles));
+  }
+    };
 
-    openTask.addEventListener("click", () => {
-         cardTask.classList.add("open");
-     });
-     
-    closeTask.addEventListener("click", () => {
-        cardTask.classList.remove("open");
-    });
+  const Duedate = [];
+  let currentDuedates = JSON.parse(localStorage.getItem('Duedate'));
+  
+  if (currentDuedates == null) {
+       currentDuedates = [];
+  };
+  
+  if (currentDuedates.length >= 5) {
+       currentDuedates.length = 0;
+  };
+  
+  const newDuedate= document.getElementById('datepicker-1').value;
+  if (newDuedate == "") {
+    submit.preventDefault();
+    alert("Please enter your Task Due Date");}
+  
+    else {
+  
+  if (currentDuedates === null) {
+      Duedate.push(newDuedate);
+    localStorage.setItem('Duedate', JSON.stringify(Users));
+  
+  } else {
+  currentDuedates.push(newDuedate);
+  localStorage.setItem('Duedate', JSON.stringify(currentDuedates));
+  }
+    };
+
+  const Description = [];
+  let currentDescription = JSON.parse(localStorage.getItem('Description'));
+  if (currentDescription == null) {
+    currentDescription = [];
+  }
+  
+  if (currentDescription.length >= 5) {
+    currentDescription.length = 0;
+  };
+  
+  const newDescription= document.getElementById('descriptionInput').value;
+  if (newDescription == "") {
+    submit.preventDefault();
+    alert("Please enter your Task Description");} 
+  
+    else {
+  if (currentDescription === null) {
+      Description.push(newDescription);
+    localStorage.setItem('Content', JSON.stringify(Description));
+  } 
+  else {
+    currentDescription.push(newDescription);
+  localStorage.setItem('Description', JSON.stringify(currentDescription));
+  }}  
 }
 
  // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-    const Title = [];
-    let currentTitles = JSON.parse(localStorage.getItem('Title'));
     
-    if (currentTitles == null) {
-      currentTitles = [];
-    };
-    
-    if (currentTitles.length >= 5) {
-      currentTitles.length = 0;
-    };
-    
-    const newTitle= document.getElementById('titleInput').value;
-    
-    if (newTitle == "") {
-      submit.preventDefault();
-      alert("Please enter your Task Title");}
-    
-      else {
-    if (currentTitles === null) {
-      Title.push(newTitle);
-      localStorage.setItem('Title', JSON.stringify(Title));
-    
-    } else {
-    currentTitles.push(newTitle);
-    localStorage.setItem('Title', JSON.stringify(currentTitles));
-    }
-      };
-
-
-    const Duedate = [];
-    let currentDuedates = JSON.parse(localStorage.getItem('Duedate'));
-    
-    if (currentDuedates == null) {
-         currentDuedates = [];
-    };
-    
-    if (currentDuedates.length >= 5) {
-         currentDuedates.length = 0;
-    };
-    
-    const newDuedate= document.getElementById('dateInput').value;
-    if (newDuedate == "") {
-      submit.preventDefault();
-      alert("Please enter your Task Due Date");}
-    
-      else {
-    
-    if (currentDuedates === null) {
-        Duedate.push(newDuedate);
-      localStorage.setItem('Duedate', JSON.stringify(Users));
-    
-    } else {
-    currentDuedates.push(newDuedate);
-    localStorage.setItem('Duedate', JSON.stringify(currentDuedates));
-    }
-      };
-
-    const Description = [];
-    let currentDescription = JSON.parse(localStorage.getItem('Description'));
-    if (currentDescription == null) {
-      currentDescription = [];
-    }
-    
-    if (currentDescription.length >= 5) {
-      currentDescription.length = 0;
-    };
-    
-    const newDescription= document.getElementById('descriptionInput').value;
-    if (newDescription == "") {
-      submit.preventDefault();
-      alert("Please enter your Task Description");} 
-    
-      else {
-    if (currentDescription === null) {
-        Description.push(newDescription);
-      localStorage.setItem('Content', JSON.stringify(Description));
-    } 
-    else {
-      currentDescription.push(newDescription);
-    localStorage.setItem('Description', JSON.stringify(currentDescription));
-    }}  
 }
-
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
 // create array of object for user input
@@ -128,9 +120,7 @@ function handleDrop(event, ui) {
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $( document ).ready(function () {
-    $(function () {
-        $("#my_date_picker").
-        datepicker ();
-    });
-    // call function requested on page 
-})
+  $(function() {
+    $( "#datepicker-1" ).datepicker();
+ })
+});
